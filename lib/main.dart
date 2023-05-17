959e0e0edfca74db989e8658df59187295ccbda7
lonnwolf031 <76941636+lonnwolf031@users.noreply.github.com>
2023-05-17 21:24:12 +0200

    work in progress ~

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import 'sqliteservice.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+}
+
+enum menuItems {
+  searchLocation,
+  settings
 }
 
 class MyApp extends StatelessWidget {
@@ -16,15 +22,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Memory map',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Memory map'),
@@ -34,15 +31,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,25 +46,25 @@ class _MyHomePageState extends State<MyHomePage> {
     areaLimit: const BoundingBox.world(),
   );
 
-
-
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
           actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.add_alert),
-              tooltip: 'Show Snackbar',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('This is a snackbar')));
-              },
-            ),
-          ],
+              PopupMenuButton(
+                onSelected: (value) {
+                  _onMenuItemSelected(value as int);
+                },
+                itemBuilder: (ctx) => [
+                  _buildPopupMenuItem('Search', Icons.search, 0),
+                  _buildPopupMenuItem('Upload', Icons.upload, 1),
+                  _buildPopupMenuItem('Copy', Icons.copy, 2),
+                  _buildPopupMenuItem('Exit', Icons.exit_to_app, 3),
+                ],
+              ),
+            ],
       ),
       body: Stack(
         children: <Widget>[
@@ -122,4 +110,24 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  PopupMenuItem _buildPopupMenuItem(
+      String title, IconData iconData, int position) {
+    return PopupMenuItem(
+      value: position,
+      child:  Row(
+        children: [
+          Icon(iconData, color: Colors.black),
+          Text(title),
+        ],
+      ),
+    );
+  }
+
+  _onMenuItemSelected(int value) {
+    setState(() {
+      // do something
+    });
+  }
 }
+
